388f684b142782f68af8399fd70d01fc3f460d29
hitomi.kan <tomy.k1688@ezweb.ne.jp>
2020-12-23 14:59:23 +0000

    user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  
+  include SessionsHelper
+  #Helper に定義していた logged_in? を下で使用してるけど、Controller から Helper のメソッドを使うことはデフォルトではできないから、
+  #ここでincludeする必要がある
+  
+  def require_user_logged_in
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
 end

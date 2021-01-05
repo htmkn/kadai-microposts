94d1ec7ac687d4ea051c5346c45b08d0d81657b9
hitomi.kan <tomy.k1688@ezweb.ne.jp>
2021-01-05 22:00:53 +0000

    modified

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,7 +2,7 @@ class Micropost < ApplicationRecord
   belongs_to :user
   has_many :users
   has_many :favorites
-  has_many :users, through: :favorites, source: :user
+  has_many :liked_users, through: :favorites, source: :user
   
   validates :content, presence: true, length: { maximum: 255 }
 end
